359031777d250d561b2ad372e292f70a2a3fd83e
Parth Kohli <59307125+parthkohli92@users.noreply.github.com>
2022-01-13 13:05:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,7 @@ def Summarize():
 		return render_template("index.html", result=output["summary_text"])
 	else:
 		return render_template("index.html")
+
+	
+if __name__ == '__main__':
+	app.run(debug=True)
